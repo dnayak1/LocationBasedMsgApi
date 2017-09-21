fca8fff85548b414bec1ab3e9a1dd921ecf46674
AkshayAyyanchira <aayyanch@uncc.edu>
2017-09-21 15:02:22 -0400

    bug test

--- routes/messageroutes.js
+++ routes/messageroutes.js
@@ -192,7 +192,7 @@ exports.unlockMessages = function(req,res){
           "message": message
         });
       }else{
-        console.log('Querying the database...');
+        console.log('Querying the database..for receiver' +receiver+'and region = '+region);
         connection.query('update Message set isLocked=0 where receiver=? AND region = ?',[receiver,region], function (error, results, fields){
           if (error) {
             console.log("error ocurred",error.code);
